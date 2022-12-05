a599834a1bbe380779715debaa9dc516e7cd12f4
Daniel <danielclark.projects@gmail.com>
2022-12-05 17:56:57 +0100

    Reto 03 - Completed

---
+++ reto-03/scripts.js
@@ -0,0 +1,28 @@
+function distributeGifts(packOfGifts, reindeers) {
+  let result = 0;
+
+  const reindeersCapacity = reindeers.reduce(
+    (total, value) => total + value.length * 2,
+    0
+  );
+
+  console.log("reindeersCapacity", reindeersCapacity);
+
+  const giftsCapacity = packOfGifts.reduce(
+    (total, value) => total + value.length,
+    0
+  );
+
+  console.log("giftsCapacity", giftsCapacity);
+
+  return Math.floor(reindeersCapacity / giftsCapacity);
+}
+
+const packOfGifts = ["book", "doll", "ball"];
+const reindeers = ["dasher", "dancer"];
+
+// el pack de regalos pesa 4 + 4 + 4 = 12
+// los renos pueden llevar (2 * 6) + (2 * 6) = 24
+// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
+
+console.log(distributeGifts(packOfGifts, reindeers)); // 2
