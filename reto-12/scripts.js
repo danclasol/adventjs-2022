1dbc175c205d4cb800151f27ccda711e002e88c7
Daniel <danielclark.projects@gmail.com>
2022-12-13 11:21:25 +0100

    Reto 12 Completed

---
+++ reto-12/scripts.js
@@ -0,0 +1,38 @@
+function selectSleigh(distance, sleighs) {
+  const BATERY_LIMIT = 20;
+
+  const sleighsFiltered = sleighs.filter(
+    (item) => distance * item.consumption <= BATERY_LIMIT
+  );
+
+  console.log(sleighsFiltered);
+
+  const sleighsSorted = sleighsFiltered.sort(
+    (a, b) => b.consumption * distance - a.consumption * distance
+  );
+
+  console.log(sleighsSorted);
+
+  return sleighsSorted[0].name;
+}
+
+const distance = 30;
+const sleighs = [
+  { name: "Dasher", consumption: 0.3 },
+  { name: "Dancer", consumption: 0.5 },
+  { name: "Rudolph", consumption: 0.7 },
+  { name: "Midu", consumption: 1 },
+];
+
+console.log(selectSleigh(distance, sleighs)); // => "Dancer"
+
+// Dasher consume 9W para recorrer 30 de distancia
+// Dancer consume 15W para recorrer 30 de distancia
+// Rudolph consume 21W para recorrer 30 de distancia
+// Midu consume 30W para recorrer 30 de distancia
+
+// El mejor trineo con el que puede recorrer
+// la distancia es Dancer.
+
+// Dasher recorre la distancia pero es peor trineo
+// Rudolph y Midu no pueden recorrer la distancia con 20W.
